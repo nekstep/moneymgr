e86f689933cafa3a44e4ae4f2e18351a6b9ec879
Konstantin Stepanenkov <me@nekstep.net>
2023-03-28 22:18:15 +0300

    Implemented toggling of admin status in users.html.

--- src/main/java/com/cfcons/moneymgr/entity/User.java
+++ src/main/java/com/cfcons/moneymgr/entity/User.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -37,4 +38,26 @@ public class User {
             inverseJoinColumns = {@JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")})
     private List<Role> roles = new ArrayList<>();
 
+    public void addRole(Role role) {
+        if (roles.stream().noneMatch(x -> x.equals(role))) {
+            roles.add(role);
+        }
+    }
+
+    public void deleteRole(Role role) {
+        roles = roles.stream().filter(x -> !x.equals(role)).collect(Collectors.toList());
+    }
+
+    public void updateRole(Role role, Boolean value) {
+        if (value) {
+            addRole(role);
+        } else {
+            deleteRole(role);
+        }
+    }
+
+    public Boolean hasRole(Role role) {
+        return roles.stream()
+                .anyMatch(x -> x.equals(role));
+    }
 }
